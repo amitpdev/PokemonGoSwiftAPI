a8c6948866dd62063e26f1d532cc4d6eeae48f81
Amit Palomo <apalgm@gmail.com>
2016-07-19 12:40:37 +0300

    Initial Commit

---
+++ PokemonGoSwiftAPI/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PokemonGoSwiftAPI
+//
+//  Created by Amit Palomo on 19/07/2016.
+//  Copyright © 2016 Amit Palomo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
